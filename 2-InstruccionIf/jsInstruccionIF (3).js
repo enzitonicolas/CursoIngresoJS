f26e3b1d8515277b7b377964871476c1892e8353
enzitonicolas <enzocruz25.ec@gmail.com>
2017-06-30 22:13:59 -0300

    If EJERCICIO 2 DEL 1 AL 6

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,14 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad
+edad=document.getElementById("edad").value;
+if(edad>=18)
+alert("mayor de edad")
 
+var edad
+edad=document.getElementById("edad").value;
+if(edad<18)
+alert("menor de edad")
 
 }//FIN DE LA FUNCIÓN
