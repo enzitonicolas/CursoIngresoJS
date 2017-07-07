44feaa702f99d5fee9475ac7ed99ba827b5126ab
enzitonicolas <enzocruz25.ec@gmail.com>
2017-07-07 03:47:24 -0300

    Switch Completo

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -2,13 +2,13 @@ function Mostrar()
 {
 //tomo la edad  
 var edad;
-var ecivil;
 edad=document.getElementById("edad").value;
-ecivil=document.getElementById("estadoCivil").value;
-{if (edad<18 && ecivil!="Soltero")}
+var estadoCivil= document.getElementById('estadoCivil').value;
 
-	{if(edad>18 && ecivil=="Soltero")
-    alert("Es soltero y no es menor")}
+if (edad < 18 && estadoCivil != 'Soltero'){
+}else if (estadoCivil=='Soltero' && edad >= 18) {
+    alert('Es soltero y no es menor');
+}
 
 
 }//FIN DE LA FUNCIÓN
--- 8-TPs/jsAdivinaElNumero1.js
+++ 8-TPs/jsAdivinaElNumero1.js
@@ -8,38 +8,39 @@ secreto del 1 al 100, en la pantalla del juego
 de no ser igual se debe informar si “falta…”  para llegar al número secreto  o si “se pasó…”  del número secreto.
 */
 var numeroSecreto; 
-var contadorIntentos;
-var numero;
+var contadorIntentos=0;
+var numero
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
 	 
 		//alert(numeroSecreto );
 
-numeroSecreto=Math.floor(Math.random()*100 + 1);
- console.log(numeroSecreto);
-}
+  numeroSecreto= Math.floor(Math.random()*100)+1;
+  console.log(numeroSecreto);
 
+}
 
 function verificar()
-{
-numero=document.getElementById("numero").value;
-contadorIntentos=contadorIntentos+1;
-document.getElementById("intentos").value=contadorIntentos
-if(numero==numeroSecreto)
-{
-alert("usted es el ganador con"+ contadorIntentos + "intentos")
-contadorIntentos=0
-}
-	 else
-   if(numeroSecreto>numero)
 
-  {
-    alert("falta" + (numeroSecreto - numero));
-}
-  else 
-  {
-    alert("se pasa")
+
+{
+  numero= document.getElementById("numero").value;
+	contadorIntentos=contadorIntentos+1;
+  document.getElementById("intentos").value=contadorIntentos
+  if(numero==numeroSecreto)
+  {   
+      alert("Usted es el ganador con "+contadorIntentos + "intentos");
+      contadorIntentos=0; 
   }
-	
+  else
+
+      if(numeroSecreto>numero)
+{
+      alert("falta"+(numeroSecreto-numero));
+}
+else
+{
+  alert("se pasa");
+}
 }
